ce1efc00cfb12408483a3a16d0d55fee6a32966a
Mathias Buus <mathiasbuus@gmail.com>
2014-01-16 21:49:00 +0100

    refactored a lot of things

--- index.js
+++ index.js
@@ -1,152 +1,36 @@
 var http = require('http');
 var Connection = require('ssh2');
+var eos = require('end-of-stream');
 var thunky = require('thunky');
-var pump = require('pump');
 var once = require('once');
-var stream = require('stream');
 var fs = require('fs');
 var path = require('path');
 var net = require('net');
-var util = require('util');
+var tcpish = require('./tcpish');
 
 var HOME = process.env.HOME || process.env.USERPROFILE;
+var ID_RSA;
+
+try {
+	ID_RSA = fs.readFileSync(path.join(HOME, '.ssh', 'id_rsa'));
+} catch (err) {
+	ID_RSA = null;
+}
+
 var noop = function() {};
 
-var Socket = function(opts) {
-	var self = this;
-
-	stream.Duplex.call(this, opts);
-
-	this.input = new stream.PassThrough();
-	this.output = new stream.PassThrough();
-	this.destroyed = false;
-	this.reading = false;
-	this.refed = true;
-
-	this.output.on('readable', function() {
-		if (self.reading) self._read();
-	});
-
-	this.output.on('end', function() {
-		self.push(null);
-	});
-
-	this.on('finish', function() {
-		self.input.end();
-	});
-
-	this.input.destroy = this.output.destroy = this.destroy.bind(this);
+var encrypted = function(key) {
+	return key && key.toString().indexOf('encrypted') > -1;
 };
 
-util.inherits(Socket, stream.Duplex);
+var agent = function(host, opts) {
+	if (typeof host === 'object' && host) return agent(null, host);
+	if (!opts) opts = {};
 
-Socket.prototype.onunref = Socket.prototype.onref = Socket.prototype.ondata = Socket.prototype.onend = noop;
+	var hwm = opts.highWaterMark;
+	var a = new http.Agent();
+	var refs = 0;
 
-Socket.prototype.setNoDelay = noop;
-
-Socket.prototype.ref = function() {
-	if (this.refed) return;
-	this.refed = true;
-	if (this.onref) this.onref();
-};
-
-Socket.prototype.unref = function() {
-	if (!this.refed) return;
-	this.refed = false;
-	if (this.onunref) this.onunref();
-};
-
-Socket.prototype.destroy = function(err) {
-	if (this.destroyed) return;
-	this.destroyed = true;
-
-	var self = this;
-	process.nextTick(function() {
-		if (err) self.emit('error', err);
-		self.emit('close');
-		self.input.emit('close');
-		self.output.emit('close');
-	});
-};
-
-Socket.prototype._write = function(data, enc, cb) {
-	return this.input.write(data, enc, cb);
-};
-
-Socket.prototype._read = function() {
-	this.reading = false;
-	var data = this.output.read();
-	if (data) this.push(data);
-	else this.reading = true;
-};
-
-var SSHWrap = function(conn) { // Wraps the rough parts of ssh2 streams
-	var self = this;
-
-	this.conn = conn;
-	this.ondrain = noop;
-	this.flushed = false;
-
-	var paused = false;
-	var update = function() {
-		if (self.flushed) {
-			if (!paused) return;
-			paused = false;
-			conn.resume();
-		} else {
-			if (paused) return;
-			paused = true;
-			conn.pause();
-		}
-	};
-
-	conn.on('drain', function() {
-		var ondrain = self.ondrain;
-		self.ondrain = noop;
-		ondrain();
-	});
-
-	conn.on('data', function(data) {
-		self.flushed = self.push(data);
-		if (self.flushed) return;
-		process.nextTick(update);
-	});
-
-	conn.on('end', function() {
-		self.push(null);
-	});
-
-	conn.on('close', function() {
-		self.emit('close');
-	});
-
-	conn.on('error', function(err) {
-		self.emit('error', err);
-	});
-
-	this.update = update;
-
-	this.on('finish', function() {
-		conn.end();
-	});
-
-	stream.Duplex.call(this);
-};
-
-util.inherits(SSHWrap, stream.Duplex);
-
-SSHWrap.prototype._write = function(data, enc, cb) {
-	if (!this.conn.writable) return cb();
-	if (this.conn.write(data) !== false) return cb();
-	this.ondrain = cb;
-};
-
-SSHWrap.prototype._read = function() {
-	this.flushed = true;
-	process.nextTick(this.update);
-};
-
-var toConnect = function(host, opts) {
 	if (host) {
 		var parts = host.match(/^(?:([^@]+)@)?([^:]+)(?::(\d+))?$/);
 		opts.username = parts[1] || 'root';
@@ -154,42 +38,48 @@ var toConnect = function(host, opts) {
 		opts.port = parseInt(parts[3] || 22, 10);
 	}
 
-	opts.privateKey = opts.key || opts.privateKey || path.join('~', '.ssh', 'id_rsa');
+	opts.privateKey = opts.key || opts.privateKey || (!encrypted(ID_RSA) || opts.passphrase ? ID_RSA : null);
 	opts.agent = opts.agent !== false && opts.agent || process.env.SSH_AUTH_SOCK;
 
-	return function() {
-		var c = new Connection();
-
-		if (Buffer.isBuffer(opts.privateKey)) return ready();
-
-		fs.readFile(opts.privateKey.replace(/^~/, HOME), function(err, key) {
-			if (err) return c.connect(opts);
-			opts.privateKey = key;
-			c.connect(opts);
-		});
-
-		return c;
-	};
-};
-
-var agent = function(host, opts) {
-	if (typeof host === 'object' && host) return agent(null, host);
-	if (!opts) opts = {};
-
-	var create = typeof host === 'function' ? host : toConnect(host, opts);
-	var hwm = opts.highWaterMark;
-	var a = new http.Agent();
-	var refs = 0;
-
+	var verified = false;
 	var connect = thunky(function loop(cb) {
-		var c = create();
+		var c = new Connection();
+		var fingerprint;
+
+		opts.hostHash = 'md5';
+		opts.hostVerifier = function(hash) {
+			fingerprint = hash;
+
+			if (verified || !opts.verify) return true;
+			if (fingerprint === opts.verify) return verified = true;
+
+			c.emit('error', new Error('Host could not be verified'));
+			return false;
+		};
+
+		var update = function() {
+			var sock = c._sock;
+			var pinger = c._pinger;
+
+			if (refs) {
+				if (sock && sock.ref) sock.ref();
+				if (pinger && pinger.ref) pinger.ref();
+			} else {
+				if (sock && sock.unref) sock.unref();
+				if (pinger && pinger.unref) pinger.unref();
+			}
+		};
 
 		var done = once(function(err) {
 			if (err) return cb(err);
-			cb(null, c);
+			verified = true;
+			cb(null, c, update);
 		});
 
-		c.on('ready', done);
+		c.on('ready', function() {
+			if (verified) return done();
+			if (!c.emit('verify', fingerprint, done)) done();
+		});
 
 		c.on('error', function(err) {
 			c.end();
@@ -199,26 +89,20 @@ var agent = function(host, opts) {
 		c.on('close', function() {
 			connect = thunky(loop);
 		});
+
+		if (typeof opts.privateKey !== 'string' || opts.privateKey.indexOf('\n') > -1) return c.connect(opts);
+
+		fs.readFile(opts.privateKey, function(_, buf) {
+			opts.privateKey = buf;
+			c.connect(opts);
+		});
 	});
 
 	a.createConnection = function(opts) {
-		var socket = new Socket(hwm ? {highWaterMark:hwm} : {});
+		var socket = tcpish(hwm ? {highWaterMark:hwm} : {});
 
-		connect(function(err, con) {
+		connect(function(err, con, update) {
 			if (err) return socket.destroy(err);
-
-			var update = function() {
-				var sock = con._sock;
-				var pinger = con._pinger;
-
-				if (refs) {
-					if (sock && sock.ref) sock.ref();
-					if (pinger && pinger.ref) pinger.ref();
-				} else {
-					if (sock && sock.unref) sock.unref();
-					if (pinger && pinger.unref) pinger.unref();
-				}
-			};
 
 			socket.onref = function() {
 				refs++;
@@ -238,7 +122,8 @@ var agent = function(host, opts) {
 					socket.destroy(err);
 					socket.unref();
 				} else {
-					pump(socket.input, new SSHWrap(stream), socket.output, function() {
+					socket.connect(stream);
+					eos(stream, function() {
 						socket.unref();
 					});
 				}
