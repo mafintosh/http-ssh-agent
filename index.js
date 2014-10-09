fb5d79c9cf74e88201edaad87e03481d70bfd268
Mathias Buus <mathiasbuus@gmail.com>
2014-10-09 11:51:58 +0800

    update style

--- index.js
+++ index.js
@@ -1,170 +1,170 @@
-var http = require('http');
-var Connection = require('ssh2');
-var eos = require('end-of-stream');
-var thunky = require('thunky');
-var once = require('once');
-var fs = require('fs');
-var path = require('path');
-var net = require('net');
-var tcpish = require('./tcpish');
+var http = require('http')
+var Connection = require('ssh2')
+var eos = require('end-of-stream')
+var thunky = require('thunky')
+var once = require('once')
+var fs = require('fs')
+var path = require('path')
+var net = require('net')
+var tcpish = require('./tcpish')
 
-var HOME = process.env.HOME || process.env.USERPROFILE;
-var ID_RSA;
+var HOME = process.env.HOME || process.env.USERPROFILE
+var ID_RSA
 
 try {
-	ID_RSA = fs.readFileSync(path.join(HOME, '.ssh', 'id_rsa'));
+  ID_RSA = fs.readFileSync(path.join(HOME, '.ssh', 'id_rsa'))
 } catch (err) {
-	ID_RSA = null;
+  ID_RSA = null
 }
 
-var noop = function() {};
+var noop = function() {}
 
 var encrypted = function(key) {
-	return key && key.toString().toLowerCase().indexOf('encrypted') > -1;
-};
+  return key && key.toString().toLowerCase().indexOf('encrypted') > -1
+}
 
 var agent = function(host, opts) {
-	if (typeof host === 'object' && host) return agent(null, host);
-	if (!opts) opts = {};
+  if (typeof host === 'object' && host) return agent(null, host)
+  if (!opts) opts = {}
 
-	var connectTimeout = typeof opts.timeout === 'number' ? opts.timeout : 15000;
-	var hwm = opts.highWaterMark;
-	var a = new http.Agent();
-	var refs = 0;
+  var connectTimeout = typeof opts.timeout === 'number' ? opts.timeout : 15000
+  var hwm = opts.highWaterMark
+  var a = new http.Agent()
+  var refs = 0
 
-	if (host) {
-		var parts = host.match(/^(?:([^@]+)@)?([^:]+)(?::(\d+))?$/);
-		opts.username = parts[1] || 'root';
-		opts.host = parts[2];
-		opts.port = parseInt(parts[3] || 22, 10);
-	}
+  if (host) {
+    var parts = host.match(/^(?:([^@]+)@)?([^:]+)(?::(\d+))?$/)
+    opts.username = parts[1] || 'root'
+    opts.host = parts[2]
+    opts.port = parseInt(parts[3] || 22, 10)
+  }
 
-	opts.privateKey = opts.key || opts.privateKey || (!encrypted(ID_RSA) || opts.passphrase ? ID_RSA : null);
-	opts.agent = opts.agent !== false && opts.agent || process.env.SSH_AUTH_SOCK;
+  opts.privateKey = opts.key || opts.privateKey || (!encrypted(ID_RSA) || opts.passphrase ? ID_RSA : null)
+  opts.agent = opts.agent !== false && opts.agent || process.env.SSH_AUTH_SOCK
 
-	var conn;
-	var verified = false;
-	var fingerprint;
+  var conn
+  var verified = false
+  var fingerprint
 
-	var connect = thunky(function loop(cb) {
-		conn = new Connection();
+  var connect = thunky(function loop(cb) {
+    conn = new Connection()
 
-		opts.hostHash = 'md5';
-		opts.hostVerifier = function(hash) {
-			fingerprint = hash;
+    opts.hostHash = 'md5'
+    opts.hostVerifier = function(hash) {
+      fingerprint = hash
 
-			if (!opts.fingerprint) return true;
-			if (fingerprint === opts.fingerprint) return true;
+      if (!opts.fingerprint) return true
+      if (fingerprint === opts.fingerprint) return true
 
-			conn.emit('error', new Error('Host could not be verified'));
-			return false;
-		};
+      conn.emit('error', new Error('Host could not be verified'))
+      return false
+    }
 
-		var update = function() {
-			var sock = conn._sock;
-			var pinger = conn._pinger;
+    var update = function() {
+      var sock = conn._sock
+      var pinger = conn._pinger
 
-			if (refs) {
-				if (sock && sock.ref) sock.ref();
-				if (pinger && pinger.ref) pinger.ref();
-			} else {
-				if (sock && sock.unref) sock.unref();
-				if (pinger && pinger.unref) pinger.unref();
-			}
-		};
+      if (refs) {
+        if (sock && sock.ref) sock.ref()
+        if (pinger && pinger.ref) pinger.ref()
+      } else {
+        if (sock && sock.unref) sock.unref()
+        if (pinger && pinger.unref) pinger.unref()
+      }
+    }
 
-		var done = once(function(err) {
-			if (err) return cb(err);
-			cb(null, conn, update);
-		});
+    var done = once(function(err) {
+      if (err) return cb(err)
+      cb(null, conn, update)
+    })
 
-		var onverify = function(err) {
-			if (err) return done(err);
-			opts.fingerprint = fingerprint;
-			done();
-		};
+    var onverify = function(err) {
+      if (err) return done(err)
+      opts.fingerprint = fingerprint
+      done()
+    }
 
-		conn.on('ready', function() {
-			if (fingerprint === opts.fingerprint) return done();
-			if (!a.emit('verify', fingerprint, onverify)) done();
-		});
+    conn.on('ready', function() {
+      if (fingerprint === opts.fingerprint) return done()
+      if (!a.emit('verify', fingerprint, onverify)) done()
+    })
 
-		conn.on('error', function(err) {
-			conn.end();
-			done(err);
-		});
+    conn.on('error', function(err) {
+      conn.end()
+      done(err)
+    })
 
-		conn.on('close', function() {
-			connect = thunky(loop);
-			done(new Error('Connection closed'));
-		});
+    conn.on('close', function() {
+      connect = thunky(loop)
+      done(new Error('Connection closed'))
+    })
 
-		if (typeof opts.privateKey !== 'string' || opts.privateKey.indexOf('\n') > -1) return conn.connect(opts);
+    if (typeof opts.privateKey !== 'string' || opts.privateKey.indexOf('\n') > -1) return conn.connect(opts)
 
-		fs.readFile(opts.privateKey, function(_, buf) {
-			opts.privateKey = buf;
-			conn.connect(opts);
-		});
-	});
+    fs.readFile(opts.privateKey, function(_, buf) {
+      opts.privateKey = buf
+      conn.connect(opts)
+    })
+  })
 
-	a.createConnection = function(opts) {
-		var socket = tcpish(hwm ? {highWaterMark:hwm} : {});
+  a.createConnection = function(opts) {
+    var socket = tcpish(hwm ? {highWaterMark:hwm} : {})
 
-		var destroy = function() {
-			if (conn && conn._sock) conn._sock.destroy();
-			else if (conn) conn.end();
-			socket.destroy();
-		};
+    var destroy = function() {
+      if (conn && conn._sock) conn._sock.destroy()
+      else if (conn) conn.end()
+      socket.destroy()
+    }
 
-		var timeout = connectTimeout && setTimeout(destroy, connectTimeout);
-		if (timeout && timeout.unref) timeout.unref();
+    var timeout = connectTimeout && setTimeout(destroy, connectTimeout)
+    if (timeout && timeout.unref) timeout.unref()
 
-		connect(function(err, con, update) {
-			if (err) {
-				clearTimeout(timeout);
-				return socket.destroy(err);
-			}
+    connect(function(err, con, update) {
+      if (err) {
+        clearTimeout(timeout)
+        return socket.destroy(err)
+      }
 
-			socket.onref = function() {
-				refs++;
-				update();
-			};
+      socket.onref = function() {
+        refs++
+        update()
+      }
 
-			socket.onunref = function() {
-				refs--;
-				update();
-			};
+      socket.onunref = function() {
+        refs--
+        update()
+      }
 
-			if (socket.refed) socket.onref();
-			else update();
+      if (socket.refed) socket.onref()
+      else update()
 
-			con.forwardOut('127.0.0.1', 8000, opts.host, opts.port, function(err, stream) {
-				clearTimeout(timeout);
+      con.forwardOut('127.0.0.1', 8000, opts.host, opts.port, function(err, stream) {
+        clearTimeout(timeout)
 
-				if (err) {
-					socket.destroy(err);
-					socket.unref();
-				} else {
-					socket.connect(stream);
-					eos(stream, function() {
-						socket.unref();
-					});
-				}
-			});
-		});
+        if (err) {
+          socket.destroy(err)
+          socket.unref()
+        } else {
+          socket.connect(stream)
+          eos(stream, function() {
+            socket.unref()
+          })
+        }
+      })
+    })
 
-		socket.on('data', function(data) {
-			if (socket.ondata) socket.ondata(data, 0, data.length);
-		});
+    socket.on('data', function(data) {
+      if (socket.ondata) socket.ondata(data, 0, data.length)
+    })
 
-		socket.on('end', function() {
-			if (socket.onend) socket.onend();
-		});
+    socket.on('end', function() {
+      if (socket.onend) socket.onend()
+    })
 
-		return socket;
-	};
+    return socket
+  }
 
-	return a;
-};
+  return a
+}
 
-module.exports = agent;
+module.exports = agent
--- tcpish.js
+++ tcpish.js
@@ -1,157 +1,157 @@
-var stream = require('stream');
-var util = require('util');
+var stream = require('stream')
+var util = require('util')
 
-var noop = function() {};
+var noop = function() {}
 
 var Tcpish = function(opts) { // Wraps the rough parts of ssh2 streams
-	stream.Duplex.call(this, opts);
+  stream.Duplex.call(this, opts)
 
-	this.ondrain = noop;
-	this.flushed = true;
-	this.conn = null;
-	this.update = noop;
-	this.buffer = null;
-	this.cb = noop;
-	this.refed = true;
-	this.destroyed = false;
-	this.finished = false;
-	this.ticks = 0;
-	this.timeout = null;
+  this.ondrain = noop
+  this.flushed = true
+  this.conn = null
+  this.update = noop
+  this.buffer = null
+  this.cb = noop
+  this.refed = true
+  this.destroyed = false
+  this.finished = false
+  this.ticks = 0
+  this.timeout = null
 
-	this.on('finish', function() {
-		if (this.timeout) clearInterval(this.timeout);
-		this.finished = true;
-		if (this.conn) this.conn.end();
-	});
-};
+  this.on('finish', function() {
+    if (this.timeout) clearInterval(this.timeout)
+    this.finished = true
+    if (this.conn) this.conn.end()
+  })
+}
 
-util.inherits(Tcpish, stream.Duplex);
+util.inherits(Tcpish, stream.Duplex)
 
-Tcpish.prototype.ondata = noop;
-Tcpish.prototype.onend = noop;
+Tcpish.prototype.ondata = noop
+Tcpish.prototype.onend = noop
 
-Tcpish.prototype.onref = noop;
-Tcpish.prototype.onunref = noop;
+Tcpish.prototype.onref = noop
+Tcpish.prototype.onunref = noop
 
-Tcpish.prototype.setNoDelay = noop;
+Tcpish.prototype.setNoDelay = noop
 
 Tcpish.prototype.ref = function() {
-	if (this.refed) return;
-	this.refed = true;
-	this.onref();
-};
+  if (this.refed) return
+  this.refed = true
+  this.onref()
+}
 
 Tcpish.prototype.unref = function() {
-	if (!this.refed) return;
-	this.refed = false;
-	this.onunref();
-};
+  if (!this.refed) return
+  this.refed = false
+  this.onunref()
+}
 
 Tcpish.prototype.destroy = function(err) {
-	if (this.destroyed) return;
-	this.destroyed = true;
-	if (this.timeout) clearInterval(this.timeout);
-	if (err) this.emit('error', err);
-	if (this.conn) this.conn.destroy();
-	this.emit('close');
-};
+  if (this.destroyed) return
+  this.destroyed = true
+  if (this.timeout) clearInterval(this.timeout)
+  if (err) this.emit('error', err)
+  if (this.conn) this.conn.destroy()
+  this.emit('close')
+}
 
 Tcpish.prototype.setTimeout = function(ms, cb) {
-	clearInterval(this.timeout);
-	if (!ms) return;
-	if (cb) this.on('timeout', cb);
+  clearInterval(this.timeout)
+  if (!ms) return
+  if (cb) this.on('timeout', cb)
 
-	var prev = this.ticks;
-	this.timeout = setInterval(function() {
-		if (prev !== self.ticks) return prev = self.ticks;
-		clearInterval(self.timeout);
-		self.emit('timeout');
-	}, ms);
+  var prev = this.ticks
+  this.timeout = setInterval(function() {
+    if (prev !== self.ticks) return prev = self.ticks
+    clearInterval(self.timeout)
+    self.emit('timeout')
+  }, ms)
 
-	if (this.timeout.unref) this.timeout.unref();
-};
+  if (this.timeout.unref) this.timeout.unref()
+}
 
 Tcpish.prototype.connect = function(conn) {
-	var self = this;
+  var self = this
 
-	this.ondrain = noop;
-	this.flushed = false;
-	this.conn = conn;
+  this.ondrain = noop
+  this.flushed = false
+  this.conn = conn
 
-	var paused = false;
-	var update = function() {
-		if (self.flushed) {
-			if (!paused) return;
-			paused = false;
-			conn.resume();
-		} else {
-			if (paused) return;
-			paused = true;
-			conn.pause();
-		}
-	};
+  var paused = false
+  var update = function() {
+    if (self.flushed) {
+      if (!paused) return
+      paused = false
+      conn.resume()
+    } else {
+      if (paused) return
+      paused = true
+      conn.pause()
+    }
+  }
 
-	var ondrain = function() {
-		var ondrain = self.ondrain;
-		self.ondrain = noop;
-		ondrain();
-	};
+  var ondrain = function() {
+    var ondrain = self.ondrain
+    self.ondrain = noop
+    ondrain()
+  }
 
-	conn.on('drain', ondrain);
+  conn.on('drain', ondrain)
 
-	conn.on('data', function(data) {
-		self.ticks++;
-		self.flushed = self.push(data);
-		if (self.flushed) return;
-		process.nextTick(update);
-	});
+  conn.on('data', function(data) {
+    self.ticks++
+    self.flushed = self.push(data)
+    if (self.flushed) return
+    process.nextTick(update)
+  })
 
-	conn.on('end', function() {
-		self.push(null);
-	});
+  conn.on('end', function() {
+    self.push(null)
+  })
 
-	conn.on('close', function() {
-		self.emit('close');
-	});
+  conn.on('close', function() {
+    self.emit('close')
+  })
 
-	conn.on('error', function(err) {
-		self.emit('error', err);
-	});
+  conn.on('error', function(err) {
+    self.emit('error', err)
+  })
 
-	this.update = update;
+  this.update = update
 
-	if (this.buffer) {
-		var buffer = this.buffer;
-		var cb = this.cb;
+  if (this.buffer) {
+    var buffer = this.buffer
+    var cb = this.cb
 
-		this.cb = noop;
-		this.buffer = null;
+    this.cb = noop
+    this.buffer = null
 
-		this.conn.write(buffer);
-		cb();
-	}
+    this.conn.write(buffer)
+    cb()
+  }
 
-	if (this.finished) this.conn.end();
-	if (this.destroyed) this.conn.destroy();
-};
+  if (this.finished) this.conn.end()
+  if (this.destroyed) this.conn.destroy()
+}
 
 Tcpish.prototype._write = function(data, enc, cb) {
-	if (!this.conn) {
-		this.buffer = data;
-		this.cb = cb;
-		return;
-	}
-	this.ticks++;
-	if (!this.conn.writable) return cb();
-	if (this.conn.write(data) !== false) return cb();
-	this.ondrain = cb;
-};
+  if (!this.conn) {
+    this.buffer = data
+    this.cb = cb
+    return
+  }
+  this.ticks++
+  if (!this.conn.writable) return cb()
+  if (this.conn.write(data) !== false) return cb()
+  this.ondrain = cb
+}
 
 Tcpish.prototype._read = function() {
-	this.flushed = true;
-	process.nextTick(this.update);
-};
+  this.flushed = true
+  process.nextTick(this.update)
+}
 
 module.exports = function() {
-	return new Tcpish();
-};
+  return new Tcpish()
+}
